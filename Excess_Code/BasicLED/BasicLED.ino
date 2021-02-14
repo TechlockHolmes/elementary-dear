0ebd12cca0754355e3a4d7802b02c38511202f31
TechlockHolmes <74975896+TechlockHolmes@users.noreply.github.com>
2021-02-14 02:52:19 -0500

    Add files via upload

---
+++ Excess_Code/BasicLED/BasicLED.ino
@@ -0,0 +1,19 @@
+int led = 13; // the pin the LED is connected to
+void setup() {
+  pinMode(led, OUTPUT); // Declare the LED as an output
+}
+
+void loop() {
+  digitalWrite(led, HIGH); // Turn the LED on
+}
+
+//rowan07, March 18 2018, "Make a Simple LED Circuit", Arduino Project Hub, 
+
+/***************************************************************************************
+*    Title: Make a Simple LED Circuit
+*    Author: rowan07
+*    Date: March 18, 2018
+*    Code version: Unknown
+*    Availability: Arduino Project Hub
+*
+***************************************************************************************/
